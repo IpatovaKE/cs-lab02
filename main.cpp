41f470a9a650bfbd00a1efcf6bcbce7b6b229f1a
Alice(IpatovaKE) <ipatova.kk.19@gmail.com>
2022-03-22 02:47:10 +0300

    code: деление чисел

--- main.cpp
+++ main.cpp
@@ -10,5 +10,6 @@ int main()
     cout << "Summ a+b=" << a+b << endl;
     cout << "Diff a-b=" << a-b << endl;
     cout << "Pr a*b=" << a*b << endl;
+    cout << "Del a/b=" << a/b << endl;
     return 0;
 }
