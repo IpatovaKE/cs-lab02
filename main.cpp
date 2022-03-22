e6bb9aefa8ee7f61b5d9836c999c87fdd6e6149f
Bob(IpatovaKE) <ipatova.kk.19@gmail.com>
2022-03-22 04:11:13 +0300

    code: декорирование кода2

--- main.cpp
+++ main.cpp
@@ -10,13 +10,13 @@ int main()
     cout << "A + B = " << a + b << '\n'
      << "A - B = " << a - b << '\n'
      << "A * B = " << a * b << '\n'
-     << "A / B = " << a / b << '\n'
-     << "min:";
+     << "A / B = " << a / b << '\n';
+     cout << "min:";
       if (a < b)
         cout << a << '\n';
      else
-        cout << b << '\n'
-     << "max:";
+        cout << b << '\n';
+     cout << "max:";
       if (a > b)
         cout << a << '\n';
     else
